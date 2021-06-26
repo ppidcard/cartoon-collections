1f028a57f7da67907e8df0af3010c4851cdfebe8
ppidcard <tvxqlikei@live.cn>
2021-06-26 13:06:40 +1000

    done

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,33 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)# code an argument here
+  res = []
+  array.each_with_index {|el, i| puts "#{i+1}. #{array}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
+
+def summon_captain_planet(planeteer_calls)
+  arrnw = planeteer_calls.map do |el| 
+    "#{el.capitalize}!"
+  end
+  arrnw
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+
+
+def long_planeteer_calls(arr)
+  arr.any? {|el| el.length > 4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(arr)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  if arr.include?("cheddar") 
+    "cheddar"
+  elsif arr.include?("gouda") 
+    "gouda"
+  elsif arr.include?("camembert") 
+     "camembert"
+  else nil
+  end
 end
+
+p find_the_cheese(['cheddar', 'sssss'])
